05b2796c9d70240d0c06f0cc0604116d533b82c5
Chris Hill <chris.j.hill94@gmail.com>
2018-02-27 11:59:16 +0000

    implemented voltage check
    function to check the voltage while no current flowing, useful for cycling through batteries to check if charged battery connected

--- generalised_code/battery_discharger.cpp
+++ generalised_code/battery_discharger.cpp
@@ -6,8 +6,8 @@
 int num_battery_in_parallel = 0;
 int analog0_pins[] = {A0, A2, A4};
 int analog1_pins[] = {A1, A3, A5};
-int bjt_pins[] = {9,10,11};
-int mosfet_pins[] = {2,3,4, 5,6,7, 8,12,13};
+int bjt_pins[] = {9, 10, 11};
+int mosfet_pins[] = {2, 3, 4, 5, 6, 7, 8, 12, 13};
 
 
 
@@ -27,26 +27,47 @@ battery::battery() {
   pinMode(analog0_pin, OUTPUT);
   pinMode(analog1_pin, OUTPUT);
 
-  for (i = 0; i < QUEUE_SIZE;i++)
+  for (i = 0; i < QUEUE_SIZE; i++)
     pinMode(mosfet_pins[i], OUTPUT);
 
 
   num_battery_in_parallel++;  //increment for next instanciation
 }
 
-void battery::update_values(){
+void battery::update_values() {
 
-  if (mosfet_pin_on<0){   //if no battery is on, skip reading
+  if (mosfet_pin_on < 0) { //if no battery is on, skip reading
     return;
   }
-  
+
   analog0_reading = analogRead(analog0_pin);
   analog1_reading = analogRead(analog1_pin);
 
-//  voltage = ;
-//  current = ;
+  //  voltage = ;
+  //  current = ;
+
+}
+void battery::check_voltage() {  //for checking the voltage when the current is off
+
+  for (int i =0; i<QUEUE_SIZE;i++){//turn off all mosfets
+    digitalWrite(mosfet_pin[i],LOW);
+  }
+  analogWrite(bjt_pin, LOW); //force bjt off for reading
+
+  digitalWrite(mosfet_pin[current_mosfet], HIGH);  //turn on current mosfet
   
+  voltage = (analogRead(analog0_pin) * NOMINAL_VOLTAGE) / RANGE;  //take reading
+  
+  if (voltage < MINIMUM_VOLTAGE_TO_START_DISCHARGE) { // if value below starting voltage increment mosfet pin
+    current_mosfet = (current_mosfet + 1) % QUEUE_SIZE;   //cycle through mosfet pins
+  }
+  else 
+  discharge_this_cell = true;
+
 }
 
-void battery::config_resistance(double resistance){}
+void battery::read_inputs() {}
+void battery::set_pwm() {}
+void battery::increment_mosfet() {}
+void battery::config_resistance(double resistance) {}
 
